63da764005a645600ef2b66ac20810186e67f30d
zep1994 <snaxbags@protonmail.com>
2020-01-28 17:47:26 -0600

    Init

---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="Nav">
+            <h1>This is for Eid</h1>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/Components/Search.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+const Search = () => {
+    return (
+        <div className="Search">
+            <h1 className="Search">This is for Eid</h1>
+            <input className="SearchBar" placeholder="Search" />
+        </div>
+    )
+}
+
+export default Search
