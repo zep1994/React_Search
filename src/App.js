c99f6b02340034889644257c242551c9ad8fcfe1
Thomas Matlock <32077179+zep1994@users.noreply.github.com>
2020-01-29 11:41:49 -0600

    React Hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './Components/Navbar'
+import axios from 'axios'
 import {
   BrowserRouter as Router,
   Switch,
@@ -14,21 +15,42 @@ import Data from './Components/Data';
 import PullData from './Containers/MainData'
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-      <Navbar />
-        <Switch>
-          <Route exact path =  '/' component={Home}/>
-          <Route exact path =  '/about' component={About} />
-          <Route exact path =  '/data' component={Data} />
-          <Route exact path =  '/main' component={PullData} />
-        </Switch>
 
-        <Search />
-      </div>
+  const [data, setData] = useState({ hits: [] })
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://hn.algolia.com/api/v1/search?query=redux',
+      )
+      setData(result.data)
+    }
+    fetchData()
+  }, [])
+
+
+  return (
+      <Router>
+        <div className="App">
+        <Navbar />
+          <Switch>
+            <Route exact path =  '/' component={Home}/>
+            <Route exact path =  '/about' component={About} />
+            <Route exact path =  '/data' component={Data} />
+            <Route exact path =  '/main' component={PullData} />
+          </Switch>
+          <ul>
+            {data.hits.map(item => (
+              <li key={item.objectID}>
+                <a href={item.url}>{item.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
     </Router>
   );
 }
+
+
 
 export default App;
